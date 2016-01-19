4b212024f16ae66166c2fdaae1234b5bab18371f
Abe Anderson <aberanderson@gmail.com>
2016-01-19 15:44:31 -0500

    finish assignment 18

---
+++ greeting.rb
@@ -0,0 +1,8 @@
+def greeting
+    greeting, person1, person2, person3 = ARGV
+    puts "#{greeting} #{person1}"
+    puts "#{greeting} #{person2}"
+    puts "#{greeting} #{person3}"
+end
+
+greeting
